a684e6852a5328f140f3a2f446ca8504d393c265
Seunghyuk Oh <seunghyukoh@furiosa.ai>
2026-02-11 08:29:52 +0000

    refactor: add new configuration files for Dream model

---
+++ cfg/debug/run.sh
@@ -0,0 +1,11 @@
+uv run run_all.py eval.py --device 0 1 --cfg cfg/debug/dream_default_list.yaml
+uv run run_all.py eval.py --device 0 1 --cfg cfg/debug/dream_threshold_list.yaml
+uv run run_all.py eval.py --device 0 1 --cfg cfg/debug/dream_factor_list.yaml
+
+
+
+uv run run_all.py eval.py --device 0 1 --cfg cfg/debug/llada_1_5_default_list.yaml
+uv run run_all.py eval.py --device 0 1 --cfg cfg/debug/llada_1_5_threshold_list.yaml
+uv run run_all.py eval.py --device 0 1 --cfg cfg/debug/llada_1_5_factor_list.yaml
+
+
